cef0e52be813860c7cb8cf424dcdad0805287565
Jackie <jonofre16@hotmail.com>
2018-08-18 21:22:31 -0700

    Added visit, delete, and reverse list methods

--- list.rb
+++ list.rb
@@ -1,13 +1,10 @@
 require './node.rb'
 
 class LinkedList
-  attr_accessor :head
-  # keep the head private. Not accessible outside class.
   def initialize
     @head = nil
   end
 
-  # insert new node at beginning of list
   def insert(value)
     new_node = Node.new(value)
     if @head
@@ -37,8 +34,12 @@ class LinkedList
   def find_max
     return nil if @head.nil?
 
-    max = 0
     current = @head
+
+    return current.data if current.next == nil
+
+    max = current.data
+
     until current == nil
       if current.data > max
         max = current.data
@@ -54,6 +55,9 @@ class LinkedList
     return nil if @head.nil?
 
     current = @head
+
+    return current.data if current.next == nil
+
     min = current.data
 
     until current == nil
@@ -82,10 +86,11 @@ class LinkedList
   def find_nth_from_beginning(n)
     return nil if !number?(n)
 
-    k = self.length - 1
-    return nil if !n.between?(0,k)
     current = @head
     return current.data if n == 0
+
+    k = self.length - 1
+    return nil if !n.between?(0,k)
 
     i = 1
     current = current.next
@@ -98,6 +103,111 @@ class LinkedList
     return current.data
   end
 
+  def insert_ascending(value)
+
+    self.insert(value) if @head.nil?
+
+    new_node = Node.new(value)
+    i = @head
+
+    if self.length == 1 && value <= i.data
+      new_node.next = i
+      @head = new_node
+      return
+    elsif self.length == 1 && value > i.data
+      @head.next = new_node
+      return
+    elsif value <= i.data
+      self.insert(value)
+    end
+
+    k = i.next
+
+    while k != nil
+      if i.data == value
+        i.next = new_node
+        new_node.next = k
+        return
+      elsif value > i.data && value < k.data
+        i.next = new_node
+        new_node.next = k
+        return
+      else
+        i = i.next
+        k = k.next
+      end
+    end
+
+    i.next = new_node
+    return
+
+  end
+
+  def visit
+    return if @head.nil?
+    current = @head
+    values = []
+    while current != nil
+      values << current.data
+      current = current.next
+    end
+    print values
+  end
+
+  def delete(value)
+    return if @head.nil?
+    current = @head
+
+    if self.length == 1 && current.data == value
+      @head = nil
+      return
+    end
+
+    if current.data == value
+      @head = current.next
+      return
+    end
+
+    previous = current
+    current = previous.next
+    next_node = current.next
+
+    while next_node != nil
+      if current.data == value
+        previous.next = next_node
+        return
+      else
+        previous = previous.next
+        current = current.next
+        next_node = next_node.next
+      end
+    end
+
+    if current.data == value
+      previous.next = nil
+      return
+    else
+      return
+    end
+
+  end
+
+  def reverse
+    return if @head.nil?
+    return if @head.next.nil?
+    previous = nil
+    current = @head
+
+    while current != nil
+      temp = current.next
+      current.next = previous
+      previous = current
+      current = temp
+    end
+    @head = previous
+    return
+  end
+
   private
 
   def number?(obj)
@@ -105,26 +215,4 @@ class LinkedList
     /\A[+-]?\d+(\.[\d]+)?\z/.match(obj)
   end
 
-
-
 end
-
-
-list = LinkedList.new
-list.insert(4)
-list.insert(3)
-list.insert(2)
-list.insert(1)
-list.insert(0)
-
-#
-# list.insert(4)
-# list.insert(6)
-#
-# list.insert(7)
-
-# puts list.search('first node')
-puts list.find_nth_from_beginning(4)
-# list.insert('third node')
-# puts list.head.data
-# puts list.head.next.data
